fe91e9ffbe7c5dc7763d2798e74e93f66b952cc2
Kerem Gurbuz <grbz.kerem@gmail.com>
2024-12-14 14:30:54 +0300

    refactor(seo): enhance sitemap with changeFrequency, priority, and strong typing
    - Update "app/sitemap.ts" to include changeFrequency and priority for each URL
    - Implement strong typing using a custom Sitemap type
    - Add type definition for Languages to support potential future localization
    - Set up structure for easy addition of future pages and dynamic content
    - Adjust static and dynamic route generation to match new type requirements
    - Set appropriate changeFrequency and priority values for different page types
    - Improve sitemap quality for better search engine crawling and indexing
    - Enhance code maintainability and type safety with strict typing

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,28 +2,61 @@ import type { MetadataRoute } from 'next';
 
 import { env } from '@/config/env';
 
+type Languages<T> = {
+  [key: string]: T;
+};
+
+type Sitemap = Array<{
+  url: string;
+  lastModified?: string | Date;
+  changeFrequency?:
+    | 'always'
+    | 'hourly'
+    | 'daily'
+    | 'weekly'
+    | 'monthly'
+    | 'yearly'
+    | 'never';
+  priority?: number;
+  alternates?: {
+    languages?: Languages<string>;
+  };
+}>;
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = env.SITE_URL;
   const currentDate = new Date();
 
-  const staticRoutes = [
+  const staticRoutes: Sitemap = [
     {
       url: baseUrl,
       lastModified: currentDate,
+      changeFrequency: 'yearly', // This should be set to 'weekly' or 'monthly'
+      priority: 1,
     },
     {
       url: `${baseUrl}/portfolio`,
       lastModified: currentDate,
+      changeFrequency: 'yearly', // This should be set to 'monthly'
+      priority: 0.8,
     },
     {
       url: `${baseUrl}/about`,
       lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.7,
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.7,
     },
   ];
 
-  return staticRoutes;
+  const dynamicRoutes: Sitemap = [
+    // Add dynamic routes here
+  ];
+
+  return [...staticRoutes, ...dynamicRoutes];
 }
